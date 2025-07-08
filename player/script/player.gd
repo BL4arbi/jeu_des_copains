b4776ae6f251f973ff5a0d363f061e69283a4c93
Larbi <infotplarbirenaud@gmail.com>
2025-07-08 14:56:34 +0200

    zgeg

--- player/script/player.gd
+++ player/script/player.gd
@@ -1,11 +1,26 @@
 class_name Player extends CharacterBody2D
 
 var movespeed : float = 50.0
+var porj_path=preload("res://projectile.tscn")
 
-
-
+func Enter()-> void :
+	
+	pass
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("left","right","up","down")
-	print(direction)
 	velocity = direction * movespeed
 	move_and_slide()
+
+func _physics_process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("click"):
+		fire()	
+
+func fire():
+	var bullet = porj_path.instantiate()
+	bullet.dir = rotation
+	bullet.pos = $Node2D.global_position
+	bullet.rota=global_rotation
+	get_parent().add_child(bullet)
+	
+	
---
+++ projectile.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var pos: Vector2
+var rota: float
+var dir: float
+var speed = 500
+
+func _ready() -> void:
+	global_position = pos
+	global_rotation = rota
+	
+	# Connecter le signal de l'Area2D enfant
+	$Area2D.body_entered.connect(_on_body_entered)
+
+func _physics_process(delta: float) -> void:
+	# Déplacer la balle
+	global_position += Vector2(speed, 0).rotated(dir) * delta
+
+func _on_body_entered(body):
+	print("Collision détectée avec : ", body.name, " - Layer : ", body.collision_layer)
+	if body.name == "CharacterBody2D2":
+		print("La balle a touché l'ennemi!")
+		$Sprite2D.visible = false
+		queue_free()
